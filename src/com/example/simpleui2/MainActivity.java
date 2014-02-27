4cacf0f85c54b0e08f1558f44132d465ad71bcc8
weihang <weihang810701@gmail.com>
2014-02-27 20:09:18 +0800

    v2

--- src/com/example/simpleui2/MainActivity.java
+++ src/com/example/simpleui2/MainActivity.java
@@ -3,13 +3,25 @@ package com.example.simpleui2;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
+	private EditText editText;
+	private Button button;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		editText = (EditText) findViewById(R.id.editText1);
+		editText.setHint("Type Something...");
+		
+		
+		button = (Button) findViewById(R.id.button1);
+		button.setText("Send");
 	}
 
 	@Override
